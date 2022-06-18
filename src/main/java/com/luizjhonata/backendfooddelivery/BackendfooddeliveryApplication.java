9378ecc01cf7acf1ebea3d70ccf0e7c5482992e2
Jhonata Luiz <jhonataluiz@hotmail.com>
2022-06-18 19:40:51 -0300

    Created Project with spring io

---
+++ src/main/java/com/luizjhonata/backendfooddelivery/BackendfooddeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.luizjhonata.backendfooddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendfooddeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendfooddeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizjhonata/backendfooddelivery/BackendfooddeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizjhonata.backendfooddelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendfooddeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
